fe340cd6650a4fef7cea69cd7a631d961b0380f7
tonyomendoza <mendozatony@outlook.com>
2021-09-16 13:26:36 -0700

    Lesson 7 Building App Components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 import React from 'react';
+
+import Nav from './components/Nav';
+
 import './App.css'
 
+// arrow function
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+      <main className="App-content">
+        <span>Empty</span>
+      </main>
+    </div>
+  );
+};
+
+export default App;
