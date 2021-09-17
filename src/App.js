7f9a73c35b0c8b77c647272626637e6511cef303
tonyomendoza <mendozatony@outlook.com>
2021-09-17 13:53:58 -0700

    Lessons 10 & 11 Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Nav from './components/Nav';
 
@@ -6,14 +6,33 @@ import './App.css'
 
 // arrow function
 const App = () => {
+  const [activeTab, setActiveTab] = useState('items');
+  /// Rules of Hooks
+  /// 1. Only call hooks at the top level of your function
+  /// 2. Only call hooks from the React function components, or from custom hooks
+  /// 3. the names of custom hooks must start with "use"
+
   return (
     <div className="App">
-      <Nav />
+      <Nav
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+      />
       <main className="App-content">
-        <span>Empty</span>
+        <Content tab={activeTab} />
       </main>
     </div>
   );
+};
+
+const Content = ({tab}) => {
+  switch (tab) {
+    default:
+      case 'items':
+        return <span>the items</span>;
+      case 'cart':
+        return <span>the cart</span>;
+  }
 };
 
 export default App;
