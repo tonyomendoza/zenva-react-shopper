854fc5bda7d207348a2ef11cabcd5b28fcc812a4
tonyomendoza <mendozatony@outlook.com>
2021-09-17 14:45:09 -0700

    Lessons 13& 14 Item Component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import './App.css';
 
 // arrow function
 const App = () => {
-  const [activeTab, setActiveTab] = useState('items');
+
   /// Rules of Hooks
   /// 1. Only call hooks at the top level of your function
   /// 2. Only call hooks from the React function components, or from custom hooks
   /// 3. the names of custom hooks must start with "use"
+  const [activeTab, setActiveTab] = useState('items');
+  const [cart, setCart] = useState([]);
+
+  const addToCart = item => {
+    setCart(prevCart => [...prevCart, item]);
+  }
 
   return (
     <div className="App">
@@ -20,18 +26,21 @@ const App = () => {
         activeTab={activeTab}
         onTabChange={setActiveTab}
       />
+      <div>
+        {cart.length} items
+      </div>
       <main className="App-content">
-        <Content tab={activeTab} />
+        <Content tab={activeTab} onAddToCart={addToCart}/>
       </main>
     </div>
   );
 };
 
-const Content = ({tab}) => {
+const Content = ({tab, onAddToCart}) => {
   switch (tab) {
     default:
       case 'items':
-        return <ItemPage items={items} />;
+        return <ItemPage items={items} onAddToCart={onAddToCart} />;
       case 'cart':
         return <span>the cart</span>;
   }
--- src/components/pages/ItemPage.js
+++ src/components/pages/ItemPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Item from '../../components/Item';
 import '../../css/ItemPage.css';
 
-function ItemPage({items}) {
+function ItemPage({items, onAddToCart}) {
     return (
       <ul className="ItemPage-items">
           {items.map(item=>
                 <li key={item.id} className="ItemPage-item">
-                    {item.name}
+                    <Item item={item} onAddToCart={() => onAddToCart(item)} />
                 </li>
             )}
       </ul>
